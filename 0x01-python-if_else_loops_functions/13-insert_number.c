0ca5f53af6cafc612a66baa0a9cdee064974defb
BrandynR <BrandynR@github.com>
2019-09-12 01:33:00 +0000

    Went over in PLD, now completed

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * insert_node - insert node into sorted list
+ * @head: beginning of list
+ * @number: number to be added to list.
+ * Return: new node inserted
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *search = *head;
+
+	if (*head == NULL || search->n > number)
+	{
+		new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
+		new->n = number;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	while (search)
+	{
+		if (!search->next || search->next->n > number)
+		{
+			new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
+			new->n = number;
+			new->next = search->next;
+			search->next = new;
+			return (new);
+		}
+		search = search->next;
+	}
+	return (NULL);
+}
