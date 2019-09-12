6cb5ae8196af7b164c49ea80922c999c220371e4
BrandynR <BrandynR@github.com>
2019-09-12 20:19:50 +0000

    Finished task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
-if (__name__ == "__main__"):
-for i in dir(hidden_4):
+
+def main():
+    for i in dir(hidden_4):
         if ("__" not in i):
             print(i)
+
+if (__name__ == "__main__"):
+    main()
