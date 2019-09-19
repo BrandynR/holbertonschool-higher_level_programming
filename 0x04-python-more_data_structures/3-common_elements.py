3e82fd4a43b05f8755b5c2b0c446962d5cc3f84d
BrandynR <BrandynR@github.com>
2019-09-19 18:40:05 +0000

    [LOCAL][ADD]Function returns a set of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-uniq_add = __import__('2-uniq_add').uniq_add
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+def common_elements(set_1, set_2):
+    return ([x for x in set_1 if x in set_2])
