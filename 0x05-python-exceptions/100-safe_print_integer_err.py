ddc95b0c0f96275c1c091c89cf36bc6eda65f184
BrandynR <BrandynR@github.com>
2019-09-30 21:21:45 +0000

    [LOCAL][ADD] Function prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,12 +4,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        if not isinstance(value, int):
-            raise ValueError("Exception: Unknown format code\
- 'd' for object of type 'str'")
         print("{:d}".format(value))
         return True
-
-    except ValueError as ve:
-        print(ve, file=sys.stderr)
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
