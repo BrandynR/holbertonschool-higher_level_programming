dae339286591af6ff3084add5eac18d054f77e2d
BrandynR <BrandynR@github.com>
2019-10-15 20:02:00 +0000

    [LOCAL][ADD] Added comments

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Class"""
 
 
 class BaseGeometry:
-
+    """Class"""
     def area(self):
+        """Calculates the area of obj"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """validates 'value' and raises exceptions"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
