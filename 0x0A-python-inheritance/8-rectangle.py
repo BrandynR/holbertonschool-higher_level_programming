2557d4c50d93407c080f4a64ca300eac23f1a634
BrandynR <BrandynR@github.com>
2019-10-15 20:03:29 +0000

    [LOCAL][ADD] Added comments

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""Class"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-
+    """Class of shape"""
     def __init__(self, width, height):
         BaseGeometry.integer_validator(self, "width", width)
         BaseGeometry.integer_validator(self, "height", height)
