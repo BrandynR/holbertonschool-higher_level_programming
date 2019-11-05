73caf79ef963fd2d552788a3812acd4f5a9fa114
BrandynR <BrandynR@github.com>
2019-11-05 18:50:24 +0000

    [Local][Add] Updated test cases

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,9 +5,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    class Square inherits from class Rectangle.
-    """
+    """Test"""
     def __init__(self, size, x=0, y=0, id=None):
         """Called when a new instance of this class is created.
         """
