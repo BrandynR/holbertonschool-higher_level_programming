74a0003572dcf24cc7d08afedc4040c54d1365ee
BrandynR <BrandynR@github.com>
2019-11-27 04:24:04 +0000

    Fixed whitespace

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,9 @@
 -- list all shows, and genres linked to that show, from database hbtn_0d_tvshows
 
-SELECT tv_shows.title, tv_genres.name FROM tv_genres
-RIGHT OUTER JOIN tv_show_genres
-ON tv_genres.id=tv_show_genres.genre_id
-RIGHT OUTER JOIN tv_shows
-ON tv_show_genres.show_id=tv_shows.id
-ORDER by tv_shows.title;
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
