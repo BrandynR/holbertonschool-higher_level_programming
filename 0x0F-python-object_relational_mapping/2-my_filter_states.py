740526db621ca492cba155687a110f4b6a787a2c
BrandynR <BrandynR@github.com>
2019-12-15 02:32:18 +0000

    displays all values in the states table of hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    row = cursor.fetchall()
+    for x in row:
+        if x[1] == argv[4]:
+            print("({}, \'{}\')".format(x[0], x[1]))
+    cursor.close()
+    db.close()
