c2c1c99eb640cdba4939f34ffef6dff7b7c98866
BrandynR <BrandynR@github.com>
2019-12-15 01:49:41 +0000

    Lists all states from a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,13 +8,21 @@ from sys import argv
 def main():
     """Lists all states from the database"""
 
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states".format(database))
-    rows = cursor.fetchall()
-    for x in rows:
-        print(x)
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3],
+    charset="utf8"
+)
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states".format(database))
+rows = cursor.fetchall()
+for x in rows:
+    print(x)
+cursor.close()
+db.close()
 
 if __name__ == "__main__":
     main()
