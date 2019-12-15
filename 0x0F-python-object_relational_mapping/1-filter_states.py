9655732ebbb94650344ca1eaf613229da25a8e77
BrandynR <BrandynR@github.com>
2019-12-15 02:26:54 +0000

    Fixed indentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-)
+    )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     row = cursor.fetchall()
