dcffff82b77251e515e041110b068fbe38d72b38
BrandynR <BrandynR@github.com>
2019-12-16 15:28:01 +0000

    Fixed whitspace

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,7 +12,6 @@ from relationship_state import Base, State
 from relationship_city import City
 
 
-
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1],
