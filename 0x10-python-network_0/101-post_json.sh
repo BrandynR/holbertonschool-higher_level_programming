1b482e59fc926dd9d1dcc1c943c09d384b967805
BrandynR <BrandynR@github.com>
2020-01-07 03:21:03 +0000

    Fixed typo

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends JSON POST request to a URL, and displays the body of the response.
-curl -sH POST "Content-Type: application/json" -d @"$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
