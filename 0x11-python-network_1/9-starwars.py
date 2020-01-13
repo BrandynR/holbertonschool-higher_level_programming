efb78f491225a94c9d458692ef76a3ef64aa3225
BrandynR <BrandynR@github.com>
2020-01-13 22:22:19 +0000

    Fixed pep8

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -5,8 +5,8 @@ script that takes in a string and sends a search request to the Star Wars API
 if __name__ == '__main__':
     import requests
     from sys import argv
-    req = requests.get('https://swapi.co/api/people', params={'search': argv[1]})
-    ppl = req.json()
+    rq = requests.get('https://swapi.co/api/people', params={'search': argv[1]})
+    ppl = rq.json()
     print("Number of results: {}".format(ppl.get('count')))
     for person in ppl.get('results'):
         print(person.get('name'))
