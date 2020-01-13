ef9ce549508fa57a349679ba8a3dc5f1e3101c02
BrandynR <BrandynR@github.com>
2020-01-13 21:59:48 +0000

    sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,11 +3,12 @@
  Python script that takes in a URL, sends a request to the URL
 and displays the body of the response.
 """
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-    req = requests.get(argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    resp = requests.get(argv[1])
+    if resp.status_code < 400:
+        print(resp.text)
     else:
-        print(req.text)
+        print("Error code: {}".format(resp.status_code))
