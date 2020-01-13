cdcc28e2e3b7d919996a2fb85613610265f51b6e
BrandynR <BrandynR@github.com>
2020-01-13 22:08:31 +0000

    script that takes in a string and sends a search request to the Star Wars API

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+script that takes in a string and sends a search request to the Star Wars API
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    req = requests.get('https://swapi.co/api/people', params={'search': argv[1]})
+    ppl = req.json()
+    print("Number of results: {}".format(ppl.get('count')))
+    for person in ppl.get('results'):
+        print(person.get('name'))
