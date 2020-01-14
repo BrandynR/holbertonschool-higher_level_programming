ca08d134f3b4b7397915afb1f5e57547d7ab85bf
BrandynR <BrandynR@github.com>
2020-01-14 01:36:18 +0000

    Pep8 fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     rget = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[2], argv[1]))
+                        .format(argv[2], argv[1]))
     commits = rget.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
